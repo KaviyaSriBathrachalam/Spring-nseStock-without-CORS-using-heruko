7ffec7fee90c24e45feb25e6339bcb34ba72e9f2
Trainee <Trainee@KGFSL.COM>
2018-05-25 18:23:08 +0530

    third commit

--- src/main/java/com/kgisl/nsedata/controller/NseController.java
+++ src/main/java/com/kgisl/nsedata/controller/NseController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 // import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import io.swagger.models.Model;
@@ -17,7 +18,7 @@ import io.swagger.models.Model;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:8080", maxAge = 3600)
-@RequestMapping("api/nse-controller")
+@RequestMapping("api/nse")
 public class NseController {
 	// NseData nse=new NseData();
 	//  nseDatas:Object;
@@ -27,11 +28,11 @@ public class NseController {
 	//  RestTemplate <restTemplate> ;
 	RestTemplate restTemplate = new RestTemplate();
 		@RequestMapping(value={"showNseData"},method={RequestMethod.GET})
-		public String showUserTable(){
-		  NseData nseDatas = restTemplate.getForObject("https://www.nseindia.com/live_market/dynaContent/live_watch/stock_watch/niftyStockWatch.json", NseData.class);  // or however I use restTemplates, havent done it yet so still fuzzy but shouldnt be too tricky.    
-		  String data = String.valueOf(nseDatas);
-		  System.out.println(data);
-		  return data;
+		public @ResponseBody String showUserTable(){
+		  String nseDatas = restTemplate.getForObject("https://www.nseindia.com/live_market/dynaContent/live_watch/stock_watch/niftyStockWatch.json", String.class);  // or however I use restTemplates, havent done it yet so still fuzzy but shouldnt be too tricky.    
+		//   String data = String.valueOf(nseDatas);
+		//   System.out.println(data);
+		  return nseDatas;
 		
 		//   model.addAttribute("nseDatas", nseDatas);
 		//   return "server"; // returns a jsp simply looping through the list and displaying.
